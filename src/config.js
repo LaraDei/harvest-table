17d0aaf331f84b9fb9eb64b5734b49fa5311df1c
Lara Samuelson <lara.samuelson@gmail.com>
2021-02-01 12:21:53 -0800

    updated endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-    API_ENDPOINT:'http://localhost:8000/api',
+    API_ENDPOINT:'https://guarded-fjord-58725.herokuapp.com/api',
     TOKEN_KEY: 'harvest-table-client-auth-token',
     USER_NAME: 'harvest-table-user-name',
     USER_ID: 'harvest-table-user-id',
